1c8b33c2134d5f821b0212877104a121e540eca6
Theedosi <dostonsuyunov18@gmail.com>
2023-03-07 15:38:51 +0500

    5

--- if05.py
+++ if05.py
@@ -6,4 +6,23 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    a1=n%10
+    n = n//10
+    a2= n%10
+    n = n//10
+    a3= n%10
+    n = n//10
+    a4= n%10
+    a5= n/10
+    
+    max = a5
+    if a4>max:
+        max=a4
+    if a3>max:
+        max=a3
+    if a2>max:
+        max=a2
+    if a1>max:
+        max=a1
+    return max
+print(main(12345))
