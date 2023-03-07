31b0a9b17f971b38046f2fb8bdb6b943ccefb3ee
Theedosi <dostonsuyunov18@gmail.com>
2023-03-07 14:40:43 +0500

    2

--- if02.py
+++ if02.py
@@ -8,4 +8,10 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    min = a
+    if b<min:
+        min = b
+    if c<min:
+        min = c
+    return min
+print(main(1,2,3))
